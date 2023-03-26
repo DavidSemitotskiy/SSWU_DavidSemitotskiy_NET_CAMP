e57272ae1762016dadbb8a75089bd2eb92b1eb2f
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-03-26 17:11:51 +0300

    added ColourMatrixExtensions

--- PixelSequences/PixelSequences/ColourSequenceInfo.cs
+++ PixelSequences/PixelSequences/ColourSequenceInfo.cs
@@ -2,8 +2,9 @@
 {
     public class ColourSequenceInfo
     {
-        public ColourSequenceInfo(int startSequence, int endSequence, int length, int colour)
+        public ColourSequenceInfo(int row, int startSequence, int endSequence, int length, int colour)
         {
+            IndexRow = row;
             StartSequence = startSequence;
             EndSequence = endSequence;
             Length = length;
@@ -14,13 +15,15 @@
 
         public int EndSequence { get; }
 
+        public int IndexRow { get; set; }
+
         public int Length { get; }
 
         public int Colour { get; }
 
         public override string ToString()
         {
-            return $"Start - {StartSequence}, End - {EndSequence}, Length - {Length}, Colour - {Colour}";
+            return $"Start - {IndexRow}{StartSequence}, End - {IndexRow}{EndSequence}, Length - {Length}, Colour - {Colour}";
         }
     }
 }
--- PixelSequences/PixelSequences/Program.cs
+++ PixelSequences/PixelSequences/Program.cs
@@ -9,78 +9,13 @@
             Console.WriteLine("Input number of cols: ");
             int columns = int.Parse(Console.ReadLine());
             int[,] colours = new int[rows, columns];
-            FillMatrixColours(colours);
-            PrintMatrixColours(colours);
-            ColourSequenceInfo[] colourSequences = new ColourSequenceInfo[rows];
-            for (int i = 0; i < rows; i++)
-            {
-                colourSequences[i] = FindLongestHorizontalSequenceColours(colours, i);
-                Console.WriteLine(colourSequences[i]);
-            }
-
-            Console.WriteLine(colourSequences.MaxBy(horizontalColourSequence => horizontalColourSequence.Length));
-        }
-
-        public static void FillMatrixColours(int[,] colours)
-        {
+            colours.FillMatrixWithColours();
+            colours.PrintMatrixOfColoursInConsole();
             Random random = new Random();
-            for (int i = 0; i < colours.GetLength(0); i++)
-            {
-                for (int j = 0; j < colours.GetLength(1); j++)
-                {
-                    colours[i, j] = random.Next(0, 16);
-                }
-            }
-        }
-
-        public static void PrintMatrixColours(int[,] matrix)
-        {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    Console.Write($"{matrix[i, j]} ");
-                }
-
-                Console.WriteLine();
-            }
-        }
-
-        public static ColourSequenceInfo FindLongestHorizontalSequenceColours(int[,] matrix, int row)
-        {
-            int startIndex = 0;
-            int endIndex = 0;
-            int length = 1;
-            int colour = matrix[row, 0];
-
-            int maxStartIndex = startIndex;
-            int maxEndIndex = endIndex;
-            int maxSequenceLength = length;
-            int maxColour = colour;
-            for (int i = 1; i < matrix.GetLength(1); i++)
-            {
-                if (matrix[row, i] != colour)
-                {
-                    startIndex = i;
-                    length = 1;
-                    colour = matrix[row, i];
-                }
-                else
-                {
-                    length += 1;
-                }
-
-                endIndex = i;
-                if (length > maxSequenceLength)
-                {
-                    maxStartIndex = startIndex;
-                    maxEndIndex = endIndex;
-                    maxColour = colour;
-                    maxSequenceLength = length;
-                }
-            }
-
-            return new ColourSequenceInfo(maxStartIndex, maxEndIndex, maxSequenceLength, maxColour);
+            var longestSequenceByRow = colours.FindLongestHorizontalSequenceByRow(random.Next(0, rows));
+            var longestSequence = colours.FindLongestHorizontalSequence();
+            Console.WriteLine($"Max sequence of colours by row {longestSequenceByRow.IndexRow}: {longestSequenceByRow}");
+            Console.WriteLine($"Max sequence of colours in a whole matrix: {longestSequence}");
         }
     }
 }
