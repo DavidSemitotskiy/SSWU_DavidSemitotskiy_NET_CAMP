65bba49633c4cea109e6304d141fd122e0ef027e
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-03-30 16:39:46 +0300

    added tensor class

---
+++ Home_task_1/exercise_4/exercise_4/Tensor.cs
@@ -0,0 +1,29 @@
+﻿namespace exercise_4
+{
+    public class Tensor<T>
+    {
+        private T[]? _arr;
+
+        private T? _value;
+
+        private TensorInfo<T> _tensorInfo;
+
+        public Tensor(TensorInfo<T> tensorInfo)
+        {
+            if (tensorInfo == null)
+            {
+                throw new ArgumentNullException("TensorInfo can't be null");
+            }
+
+            _tensorInfo = tensorInfo;
+            var tensor = _tensorInfo.PrepareTensor();
+            if (tensor is T[] arrayTensor)
+            {
+                _arr = arrayTensor;
+                return;
+            }
+
+            _value = (T)tensor;
+        }
+    }
+}
