a2e13c7a5a44ddf635d2713c66ce0a699fa9e984
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-03-30 16:57:48 +0300

    added methods to operate with value and arr

--- Home_task_1/exercise_4/exercise_4/Tensor.cs
+++ Home_task_1/exercise_4/exercise_4/Tensor.cs
@@ -1,4 +1,6 @@
-﻿namespace exercise_4
+﻿using System.Text;
+
+namespace exercise_4
 {
     public class Tensor<T>
     {
@@ -24,6 +26,58 @@
             }
 
             _value = (T)tensor;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (!_tensorInfo.IsTensorValue())
+                {
+                    throw new RankException("Tensor isn't value");
+                }
+
+                return _value;
+            }
+            set
+            {
+                if (!_tensorInfo.IsTensorValue())
+                {
+                    throw new RankException("Tensor isn't value");
+                }
+
+                _value = value;
+            }
+        }
+
+        public T this[params int[] indexes]
+        {
+            get
+            {
+                return _arr[_tensorInfo.CountIndexOffset(indexes)];
+            }
+            set
+            {
+                _arr[_tensorInfo.CountIndexOffset(indexes)] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (_tensorInfo.IsTensorValue())
+            {
+                return $"Value - {_value}";
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(_tensorInfo.ToString());
+            stringBuilder.Append("Data: ");
+            foreach (var item in _arr)
+            {
+                stringBuilder.Append($"{item} ");
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
