ffb3b2aaedc0b5051bb0fb15b61243480533d830
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-04-04 16:27:47 +0300

    added functionality for exercise_2

--- Home_task_3/exercise_2/exercise_2/Program.cs
+++ Home_task_3/exercise_2/exercise_2/Program.cs
@@ -4,6 +4,16 @@
     {
         public static void Main()
         {
+            Console.Write("Input text: ");
+            string userInput = Console.ReadLine();
+            Console.Write("Input substring: ");
+            string substring = Console.ReadLine();
+            Console.WriteLine($"Index of second including: {userInput.IndexOfSecondIncluding(substring)}");
+            Console.WriteLine($"Count of words that starts with upper letter: {userInput.CountWordsStartsWithUpperLetter()}");
+            Console.Write("Input replace string: ");
+            string replace = Console.ReadLine();
+            string resultReplacing = userInput.ReplaceAllWordsWithDoubleLetters(replace);
+            Console.WriteLine($"String after replacing: {resultReplacing}");
         }
     }
 }
---
+++ Home_task_3/exercise_2/exercise_2/TextExtensions.cs
@@ -0,0 +1,57 @@
+﻿namespace exercise_2
+{
+    public static class TextExtensions
+    {
+        public static int? IndexOfSecondIncluding(this string str, string substring)
+        {
+            int firstOffset = str.IndexOf(substring);
+            if (firstOffset == -1)
+            {
+                return null;
+            }
+
+            int result = str.IndexOf(substring, firstOffset + substring.Length);
+            return result == -1 ? null : result;
+        }
+
+        public static int CountWordsStartsWithUpperLetter(this string str)
+        {
+            string[] words = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int countWords = 0;
+            foreach (string word in words)
+            {
+                if (char.IsUpper(word[0]))
+                {
+                    countWords++;
+                }
+            }
+
+            return countWords;
+        }
+
+        public static string ReplaceAllWordsWithDoubleLetters(this string str, string replace)
+        {
+            string[] words = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            bool containsDoubleLetters = false;
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length - 1; j++)
+                {
+                    if (words[i][j] == words[i][j + 1])
+                    {
+                        containsDoubleLetters = true;
+                        break;
+                    }
+                }
+
+                if (containsDoubleLetters)
+                {
+                    words[i] = replace;
+                    containsDoubleLetters = false;
+                }
+            }
+
+            return string.Join(' ', words);
+        }
+    }
+}
