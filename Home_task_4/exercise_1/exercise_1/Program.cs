7454e348b4755fbeae486e2f4ab0f171c309d1df
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-04-15 12:24:38 +0300

    workable home_task_4 exercise 1

--- Home_task_4/exercise_1/exercise_1/Program.cs
+++ Home_task_4/exercise_1/exercise_1/Program.cs
@@ -4,6 +4,50 @@
     {
         public static void Main()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            List<string> textUkrainian = new List<string>
+            {
+                "Дано текст, який потрібно розмістити в колекцію стрічок",
+                "(можна для збереження використовувати і масив).",
+                "Текст містить (якийсь текст) речення, які не",
+                "обов’язково розташовані в одній стрічці.",
+                "Речення можуть закінчуватися",
+                "крапкою, знаком оклику та знаком запитання.",
+                "Вважати, що цих знаків",
+                "не може бути в середині речень.",
+                "У реченнях може бути текст в круглих дужках.",
+                "Потрібно знайти та видрукувати всі речення, які містять інформацію в дужках.",
+                "Вкладених круглих дужок немає.",
+                "Розв’язати задачу, не використовуючи злиття(конкатенацію) стрічок",
+                "та продемонструвати виконання для текстів українською",
+                "та англійською мовами."
+            };
+            TextSearcher textSearcher1 = new TextSearcher(textUkrainian);
+            var result1 = textSearcher1.FindRowsWithBrackets();
+            PrintText(result1);
+
+            List<string> textEnglish = new List<string>()
+            {
+                "The cat lazily(some text) stretched",
+                "out on the windowsill(some text),",
+                "basking in the warm afternoon sun.",
+                "After a long day at work,",
+                "Sarah enjoyed relaxing in her favorite armchair with",
+                "a good book and a cup of tea."
+            };
+            TextSearcher textSearcher2 = new TextSearcher(textEnglish);
+            var result2 = textSearcher2.FindRowsWithBrackets();
+            PrintText(result2);
+        }
+
+        public static void PrintText(List<string> text)
+        {
+            foreach (string s in text)
+            {
+                Console.Write($"{s} ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
---
+++ Home_task_4/exercise_1/exercise_1/TextSearcher.cs
@@ -0,0 +1,67 @@
+﻿namespace exercise_1
+{
+    public class TextSearcher
+    {
+        private List<string> _textList;
+
+        private char[] _rowSeparators = { '.', '!', '?' };
+
+        public TextSearcher(List<string> textList)
+        {
+            _textList = textList;
+        }
+
+        public List<string> FindRowsWithBrackets()
+        {
+            int indexStartRow = 0;
+            int leftBracket = 0;
+            int rightBracket = 0;
+            List<string> rowsWithBrackets = new List<string>();
+            for (int i = 0; i < _textList.Count && indexStartRow < _textList.Count; i++)
+            {
+                if (((leftBracket = _textList[i].IndexOf('(')) != -1 && (rightBracket = _textList[i].IndexOf(')')) != -1) && rightBracket > leftBracket)
+                {
+                    i = CreateRow(rowsWithBrackets, indexStartRow);
+                    indexStartRow = i + 1;
+                }
+                else if(ContainsRowSeparator(_textList[i]))
+                {
+                    indexStartRow = i + 1;
+                }
+            }
+
+            return rowsWithBrackets;
+        }
+
+        private int CreateRow(List<string> rowsWithBrackets, int indexStart)
+        {
+            bool endFound = false;
+            int indexEnd = 0;
+            for (int i = indexStart; i < _textList.Count && !endFound; i++)
+            {
+                if (ContainsRowSeparator(_textList[i]))
+                {
+                    endFound = true;
+                    indexEnd = i;
+                }
+
+                rowsWithBrackets.Add(_textList[i]);
+            }
+
+            return indexEnd;
+        } 
+
+        private bool ContainsRowSeparator(string str)
+        {
+            for (int i = 0; i < _rowSeparators.Length; i++)
+            {
+                if (str.IndexOf(_rowSeparators[i]) != -1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
