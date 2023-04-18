91fb8b3ea7c6cfa79a5c414b60368026b641e9ce
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-04-18 22:42:50 +0300

    added try catch in Program

--- Home_task_5/exercise_2/exercise_2/Program.cs
+++ Home_task_5/exercise_2/exercise_2/Program.cs
@@ -6,8 +6,15 @@
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.InputEncoding = System.Text.Encoding.Unicode;
-            ConsoleShopService service = new ConsoleShopService();
-            service.StartWork();
+            try
+            {
+                ConsoleShopService service = new ConsoleShopService();
+                service.StartWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
