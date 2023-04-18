1c99babda7a6b8278a61c489c045d0d223d7178c
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-04-18 10:25:02 +0300

    added Point class with Distance method

---
+++ Home_task_5/exercise_1/exercise_1/Point.cs
@@ -0,0 +1,20 @@
+﻿namespace exercise_1
+{
+    public class Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public static double Distance(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+        }
+    }
+}
