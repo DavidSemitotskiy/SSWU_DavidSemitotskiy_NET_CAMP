918f821a5648b74412ce0e959fee67c7bfaac0df
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-04-18 16:21:34 +0300

    created class for products

---
+++ Home_task_5/exercise_2/exercise_2/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace exercise_2
+{
+    public class Product
+    {
+        private int _height;
+
+        private int _width;
+
+        private string _name;
+
+        private string _location;
+
+        public Product(int height, int width, string name, string location)
+        {
+            _height = height;
+            _width = width;
+            _name = name;
+            _location = location;
+        }
+    }
+}
