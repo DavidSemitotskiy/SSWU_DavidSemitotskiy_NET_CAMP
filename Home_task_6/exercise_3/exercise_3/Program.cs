8b389c228bf9d3ed6a6f34aa58700c454a99ef34
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-04-28 09:29:40 +0300

    workable exercise 3 home_task_6 without refactoring

--- Home_task_6/exercise_3/exercise_3/Program.cs
+++ Home_task_6/exercise_3/exercise_3/Program.cs
@@ -2,8 +2,13 @@
 {
     public class Program
     {
-        static void Main()
+        public static void Main()
         {
+            WordSearcher wordSearcher = new WordSearcher("Jdoaw    kdau  k jdawu    la Jdoaw     k");
+            foreach (string word in wordSearcher.GetUniqueWords())
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
---
+++ Home_task_6/exercise_3/exercise_3/WordSearcher.cs
@@ -0,0 +1,39 @@
+﻿namespace exercise_3
+{
+    public class WordSearcher
+    {
+        private string _text;
+
+        public WordSearcher(string text) 
+        {
+            _text = text;
+        }
+
+        public IEnumerable<string> GetUniqueWords()
+        {
+            string text = _text;
+            text = text?.Trim();
+            (string? word, bool isSuccess) resultGettingWord;
+            while ((resultGettingWord = GetWord(text)).isSuccess)
+            {
+                if (text.IndexOf(resultGettingWord.word, resultGettingWord.word.Length) == -1)
+                {
+                    yield return resultGettingWord.word;
+                }
+
+                text = text.Replace(resultGettingWord.word, "").TrimStart();
+            }
+        }
+
+        private (string?, bool) GetWord(string text)
+        {
+            if (text == null || string.IsNullOrWhiteSpace(text))
+            {
+                return (null, false);
+            }
+
+            int indexOfEndWord = text.IndexOf(' ');
+            return indexOfEndWord == -1 ? (text, true) : (text[..indexOfEndWord], true);
+        }
+    }
+}
