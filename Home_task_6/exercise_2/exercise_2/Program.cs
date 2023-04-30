4034088676f8d635859e2fa59716f0d00af52cbb
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-04-30 20:59:04 +0300

    added functionality for sirting

--- Home_task_6/exercise_2/exercise_2/Program.cs
+++ Home_task_6/exercise_2/exercise_2/Program.cs
@@ -4,6 +4,17 @@
     {
         public static void Main(string[] args)
         {
+            Random random = new Random();
+            int[] first = Enumerable.Range(0, 5).Select(_ => random.Next(600)).ToArray();
+            int[] second = Enumerable.Range(0, 3).Select(_ => random.Next(600)).ToArray();
+            int[] third = Enumerable.Range(0, 4).Select(_ => random.Next(600)).ToArray();
+            var sorter = new Sorter();
+            foreach (int i in sorter.MergeAndSortCollections(first, second, third)) 
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
---
+++ Home_task_6/exercise_2/exercise_2/Sorter.cs
@@ -0,0 +1,14 @@
+﻿namespace exercise_2
+{
+    public class Sorter
+    {
+        public IEnumerable<int> MergeAndSortCollections(params IEnumerable<int>[] collections)
+        {
+            IEnumerable<int> joined = collections.SelectMany(x => x);
+            foreach (int item in joined.Order())
+            {
+                yield return item;
+            }
+        }
+    }
+}
