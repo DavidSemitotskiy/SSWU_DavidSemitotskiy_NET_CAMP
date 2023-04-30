9da846eb9dd1be33e8dc3761c4803c81216b349a
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2023-04-30 21:33:18 +0300

    added one more variation of sorting

--- Home_task_6/exercise_2/exercise_2/Program.cs
+++ Home_task_6/exercise_2/exercise_2/Program.cs
@@ -5,16 +5,19 @@
         public static void Main(string[] args)
         {
             Random random = new Random();
-            int[] first = Enumerable.Range(0, 5).Select(_ => random.Next(600)).ToArray();
+            int[] first = Enumerable.Range(0, 10).Select(_ => random.Next(600)).ToArray();
             int[] second = Enumerable.Range(0, 3).Select(_ => random.Next(600)).ToArray();
             int[] third = Enumerable.Range(0, 4).Select(_ => random.Next(600)).ToArray();
-            var sorter = new Sorter();
-            foreach (int i in sorter.MergeAndSortCollections(first, second, third)) 
+            foreach (int i in Sorter.MergeAndSortCollectionsLinq(first, second, third))
             {
                 Console.Write($"{i} ");
             }
 
             Console.WriteLine();
+            foreach (int i in Sorter.MergeAndSortCollections(first, second, third))
+            {
+                Console.Write($"{i} ");
+            }
         }
     }
 }
--- Home_task_6/exercise_2/exercise_2/Sorter.cs
+++ Home_task_6/exercise_2/exercise_2/Sorter.cs
@@ -1,14 +1,42 @@
 ﻿namespace exercise_2
 {
-    public class Sorter
+    public static class Sorter
     {
-        public IEnumerable<int> MergeAndSortCollections(params IEnumerable<int>[] collections)
+        public static IEnumerable<int> MergeAndSortCollectionsLinq(params IEnumerable<int>[] collections)
         {
             IEnumerable<int> joined = collections.SelectMany(x => x);
             foreach (int item in joined.Order())
             {
                 yield return item;
             }
+        }
+
+        public static IEnumerable<int> MergeAndSortCollections(params IEnumerable<int>[] collections)
+        {
+            List<int> joined = collections.SelectMany(x => x).ToList();
+            (int value, int index) minResult;
+            while (joined.Count != 0)
+            {
+                minResult = FindMinElement(joined);
+                yield return minResult.value;
+                joined.RemoveAt(minResult.index);
+            }
+        }
+
+        private static (int, int) FindMinElement(List<int> collection)
+        {
+            int min = collection[0];
+            int indexMin = 0;
+            for (int i = 1; i < collection.Count; i++)
+            {
+                if (collection[i] < min)
+                {
+                    min = collection[i];
+                    indexMin = i;
+                }
+            }
+
+            return (min, indexMin);
         }
     }
 }
